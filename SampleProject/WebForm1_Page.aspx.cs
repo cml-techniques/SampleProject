fd444a0d716064802dca75194d26088aa5c0860d
jose-joseph <jose.joseph@cmlgroup.ae>
2018-02-21 11:44:35 +0400

    Corrected page load

--- SampleProject/WebForm1_Page.aspx.cs
+++ SampleProject/WebForm1_Page.aspx.cs
@@ -11,7 +11,10 @@ namespace SampleProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Page.IsPostBack)
+            {
+                Response.Write("Corrected");
+            }
         }
     }
 }
